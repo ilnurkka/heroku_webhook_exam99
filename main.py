9911d98c2c7c147195e14e52a09e2862f81c087c
Ilnur <ilnurrismagilovv3@gmail.com>
2022-01-28 12:47:02 +0300

    commit

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ server = Flask(__name__)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
+
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo(message):
     bot.reply_to(message, message.text)
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL)
+
 
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
